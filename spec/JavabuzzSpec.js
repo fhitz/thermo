e363aff13a4933e9bd3a89c74c3aac1a5fc163d2
stefan22 <stefan.emailacct@gmail.com>
2015-07-22 23:22:55 +0100

    refactored each javascript instance on test

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,16 +2,18 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
     it('is divisible by three', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   }); //number is divisible
 
   describe('knows when a number is NOT', function() {
     it('divisible by three', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
 
