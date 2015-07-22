ed5b695632b2d8267be47a3860663f296ebcb17a
stefan22 <stefan.emailacct@gmail.com>
2015-07-22 23:59:04 +0100

    divisible by 3,5,15 in js

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -19,4 +19,38 @@ describe('Javabuzz', function() {
 
   }); //number is not divisible
 
-});
+  describe('knows when a number is', function() {
+    it('divisible by five',function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by five', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function() {
+    it('divisible by fifteen', function() {
+        expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by fifteen', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+}); //Javabuzz
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -7,3 +7,11 @@ Javabuzz.prototype._isDivisibleBy = function(number,divisor) {
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number,3);
 };
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number,5);
+};
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number,15);
+};
