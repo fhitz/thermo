1fdf262146d6fbd25fc430a0d376315fa015b87f
stefan22 <stefan.emailacct@gmail.com>
2015-07-23 15:29:24 +0100

    increase temperature test

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -2,9 +2,14 @@ describe('Thermo', function() {
 
   var thermo;
 
-  it('has a default a default temperature of 20 degrees', function() {
+  it('has a default temperature of 20 degrees', function() {
     thermo = new Thermo();
-    expect(thermo.defaultTemperature).toEqual(20);
+    expect(thermo.defaultTemperature).toBe(20);
+  });
+
+  it('increase temperature by one degree with up button', function() {
+    thermo = new Thermo();
+    expect(thermo.increaseTemperature()).toEqual(21);
   });
 
 
--- src/Thermo.js
+++ src/Thermo.js
@@ -1,3 +1,7 @@
 var Thermo = function() {
   this.defaultTemperature = 20;
 };
+
+Thermo.prototype.increaseTemperature = function() {
+  return this.defaultTemperature + 1;
+};
