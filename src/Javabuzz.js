61fd0e2da1426c60d533ff03af3a43fadd1c5859
stefan22 <stefan.emailacct@gmail.com>
2015-07-23 01:31:08 +0100

    all tests passed

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -15,3 +15,16 @@ Javabuzz.prototype.isDivisibleByFive = function(number) {
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number,15);
 };
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return "JavaBuzz";
+  }
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  }
+  return number;
+};
